f2ae101ff2eb2091de620c0bd86aaffd49dc8d2f
SergiyMilienko <106393416+SergiyMilienko@users.noreply.github.com>
2023-09-22 18:29:25 +0300

    quote generation and sharing

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
+import RandomQuote from './Components/RandomQuote/RandomQuote';
 
 function App() {
   return (
     <div>
-      
+      <RandomQuote/>
     </div>
   );
 }
